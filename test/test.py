4dafca881cb1a84af53e80a30e893989d1927c39
Jie Li <lijie_12@hotmail.com>
2024-01-20 23:32:47 +0000

    rename

---
+++ test/test.py
@@ -0,0 +1,19 @@
+"""
+Author         : Jie Li, Innovision IP Ltd and School of Mathematics, Statistics and Actuarial Science, University of Kent.
+Date           : 2024-01-20 23:30:09
+Last Revision  : 2024-01-20 23:32:45
+Last Author    : Jie Li
+File Path      : /skewt-scipy/test/test.py
+Description    :
+
+
+
+
+
+
+
+
+Copyright (c) 2024, Jie Li, jl725@kent.ac.uk
+All Rights Reserved.
+"""
+import skewt_scipy as ss
